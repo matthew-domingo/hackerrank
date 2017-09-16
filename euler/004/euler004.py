cf8a59df9fd6a2e407021c15858b4275bc0fa9c9
Matthew Domingo <31224110+matthew-domingo@users.noreply.github.com>
2017-09-16 15:19:56 -0700

    small changes to euler004

--- euler/004/euler004.py
+++ euler/004/euler004.py
@@ -4,42 +4,44 @@
 import sys
 import math
 
-def createPalindrome(val):
+def create_palindromes(val):
     strVal = str(val)
     strVal += strVal[::-1]
     return int(strVal)
 
-def is3DigitProduct(num):
+def is_3_digit_prod(num):
+    # returns True if it is a product of 2 3-digit numbers
+    # otherwise returns False
     # use maxVal to "pivot" around the square root of the number
     maxVal = math.floor(math.sqrt(num)) + 1
-    for factor1 in range(100, maxVal):
-        factor2 = num // factor1
-        if factor2 >= 100 and factor2 < 1000 and num % factor1 == 0:
+    for factor1 in range(101, maxVal):
+        factor2 = num / factor1
+        if factor2 >= 100 and factor2 < 1000 and (factor2).is_integer():
             return True
     return False
 
-def genPalindromes():
+def gen_6_digit_palindromes():
+    #creates 6 digit palindromes
     for i in range(101, 1000):
-        pali = createPalindrome(i)
-        if is3DigitProduct(pali):
-            yield pali
-
+        yield create_palindromes(i)
 
 def main():
-    palindromes = list(genPalindromes())
-    #
-    numOfTests = int(input().strip())
-    for _ in range(numOfTests):
-        N = int(input().strip())
-        maxPali = 0
-        # could do a max(p for p in palindromes if p < n), but that doesn't have a break function
-        # and palindromes is in order
+    palindromes = list(pali for pali in gen_6_digit_palindromes() if is_3_digit_prod(pali))
+    # number of tests to run
+    tests = int(input().strip())
+    for test in range(tests):
+        # test value
+        n = int(input().strip())
+        pali_max = 0
+        # could do max(p for p in palindromes if p < n),
+        # but that doesn't have a break function
+        # and palindromes is already sorted
         for p in palindromes:
-            if p >= N:
+            if p >= n:
                 break
-            if p > maxPali:
-                maxPali = p
-        print(maxPali)
+            if p > pali_max:
+                pali_max = p
+        print(pali_max)
 
 if __name__ == '__main__':
     main()
