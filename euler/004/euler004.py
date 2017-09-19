09ce38512ad079f4ddb5ea66491bbc7ddb4c5d40
Matthew Domingo <31224110+matthew-domingo@users.noreply.github.com>
2017-09-18 21:43:06 -0700

    sped up search algorithm, added comments

--- euler/004/euler004.py
+++ euler/004/euler004.py
@@ -5,14 +5,19 @@ import sys
 import math
 
 def create_palindromes(val):
+    """
+    mirrors val and returns that palindrome
+    """
     strVal = str(val)
     strVal += strVal[::-1]
     return int(strVal)
 
 def is_3_digit_prod(num):
-    # returns True if it is a product of 2 3-digit numbers
-    # otherwise returns False
-    # use maxVal to "pivot" around the square root of the number
+    """
+    returns True if it is a product of 2 3-digit numbers
+    otherwise returns False
+    uses maxVal to "pivot" around the square root of the number
+    """
     maxVal = math.floor(math.sqrt(num)) + 1
     for factor1 in range(101, maxVal):
         factor2 = num / factor1
@@ -21,27 +26,37 @@ def is_3_digit_prod(num):
     return False
 
 def gen_6_digit_palindromes():
-    #creates 6 digit palindromes
+    """creates 6 digit palindromes"""
     for i in range(101, 1000):
         yield create_palindromes(i)
 
+def search(n, palindromes):
+    """
+    returns the index number of n in palindromes
+    the value of palindromes[index] < n
+    log(n) search method
+    """
+    left = 0
+    right = len(palindromes) - 1
+    mid = right//2
+    while (mid != left):
+        if palindromes[mid] >= n:
+            right = mid
+            mid = (left + right)//2
+        if palindromes[mid] < n:
+            left = mid
+            mid = (left + right)//2
+    if palindromes[mid] >= n:
+        return mid - 1
+    return mid
+
 def main():
     palindromes = list(pali for pali in gen_6_digit_palindromes() if is_3_digit_prod(pali))
-    # number of tests to run
-    tests = int(input().strip())
+    tests = int(input().strip()) # number of tests to run
     for test in range(tests):
-        # test value
-        n = int(input().strip())
-        pali_max = 0
-        # could do max(p for p in palindromes if p < n),
-        # but that doesn't have a break function
-        # and palindromes is already sorted
-        for p in palindromes:
-            if p >= n:
-                break
-            if p > pali_max:
-                pali_max = p
-        print(pali_max)
+        n = int(input().strip()) # test value
+        pali_max_n = search(n, palindromes)
+        print(palindromes[pali_max_n])
 
 if __name__ == '__main__':
     main()
