e625c2d391a87cc9337cc62b3e63d7e567b71d94
Matthew Domingo <31224110+matthew-domingo@users.noreply.github.com>
2017-09-26 18:52:42 -0700

    minor speed boosts, still failing test 11

--- euler/093/093.py
+++ euler/093/093.py
@@ -2,7 +2,11 @@ import itertools
 # Doesn't pass all tests
 # Brute force attempt, passes all timeouts
 
-def applyOperations(val, num_of_operations, vals_found):
+def applyOperations(val, vals_found):
+    num_of_operations = len(val) - 1
+    if num_of_operations == 0 and val[0].is_integer() and val[0] >= 1:
+        vals_found.add(int(val[0]))
+        return
     for i in range(num_of_operations):
         x = [0 for i in range(4)]
         x[0] = mul(i, val)
@@ -14,11 +18,8 @@ def applyOperations(val, num_of_operations, vals_found):
             x.pop()
         if num_of_operations > 0:
             for arr in x:
-                applyOperations(arr, len(arr) - 1, vals_found)
-    if num_of_operations == 0:
-        if val[0].is_integer():
-            if val[0] >= 1:
-                vals_found.add(int(val[0]))
+                applyOperations(arr, vals_found)
+
 
 def div(index, arr):
     if index >= len(arr):
@@ -46,9 +47,11 @@ def main():
     _ = input()
     val = [float(i) for i in input().split()]
     vals_found = set()
-    for permutation in itertools.permutations(val):
+    # removes all duplicate tuples
+    permutates = set(itertools.permutations(val))
+    for permutation in permutates:
         l_perm = list(permutation)
-        applyOperations(l_perm, len(l_perm) - 1, vals_found)
+        applyOperations(l_perm, vals_found)
     vals_found_list = sorted(list(vals_found))
     index = 0
     cnt = 1
